1c6e79d169517c20351d20456e14b2dda8c6ecfe
Ashish Goyal <ashishgy77@gmail.com>
2024-01-25 18:25:48 +0530

    Combining all blocks to create a Decoder

--- transformer/model.py
+++ transformer/model.py
@@ -158,6 +158,40 @@ class Encoder(nn.Module):
 
         return self.norm(x)
 
+class DecoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block: MultiHeadAttention,
+                  cross_attention_bloc: MultiHeadAttention,
+                  feed_forward_block: FeedForwardBlock,
+                  dropout:float):
+
+        super().__init__()
+
+        self.self_attention_block = self_attention_block
+        self.cross_attention_bloc = cross_attention_bloc
+        self.feed_forward_block = feed_forward_block
+        self.resididual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.resididual_connections[0](x, lambda x : self.self_attention_block(x,x,x,tgt_mask))
+        x = self.resididual_connections[1](x, lambda x: self.cross_attention_bloc(x, encoder_output,encoder_output, src_mask))
+        x = self.resididual_connections[2](x, self.feed_forward_block)
+        return x
+
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self,x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x,encoder_output,src_mask, tgt_mask)
+
+        return self.norm(x)
+
+
 
 
 
