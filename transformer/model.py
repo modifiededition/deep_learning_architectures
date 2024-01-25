f7dcd316d05215cd9165acf9c08139d7bf440085
Ashish Goyal <ashishgy77@gmail.com>
2024-01-25 17:45:46 +0530

    Adding Residual Block

--- transformer/model.py
+++ transformer/model.py
@@ -120,6 +120,17 @@ class MultiHeadAttention(nn.Module):
 
         return self.w_o(x)
 
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout:float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self,x, sublayer):
+        return x+ self.dropout(sublayer(self.norm(x)))
+
+
 
 
 
