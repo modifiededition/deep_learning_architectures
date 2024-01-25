f8cf67e1d34801d33b178feab4a59299de21a111
Ashish Goyal <ashishgy77@gmail.com>
2024-01-25 13:11:14 +0530

    Adding Input Embedding block

---
+++ transformer/model.py
@@ -0,0 +1,15 @@
+import torch
+import math
+import torch.nn as nn
+
+class InputEmbedding(nn.Module):
+
+    def __init__(self, d_model, vocab_size):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self,x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
