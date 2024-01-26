d668552dd5a5fdf2343f91f9c4ae6ef08764b104
Ashish Goyal <ashishgy77@gmail.com>
2024-01-26 15:01:53 +0530

    Adding code for building dataset

--- transformer/train.py
+++ transformer/train.py
@@ -8,6 +8,8 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
+from dataset import BilingualDataset
+
 from pathlib import Path
 
 def get_all_sentences(ds, lang):
@@ -39,3 +41,23 @@ def get_ds(config):
     val_ds_size = len(ds_raw) - train_ds_size
 
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size,val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw,src_tokenizer,tgt_tokenizer,config["lang_src"], config["lang_tgt"],config["seq_len"])
+    val_ds = BilingualDataset(val_ds_raw,src_tokenizer,tgt_tokenizer,config["lang_src"], config["lang_tgt"],config["seq_len"])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = src_tokenizer.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tgt_tokenizer.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_ds, batch_size = config["batch_size"], shuffle= True)
+    val_dataloader = DataLoader(val_ds, batch_size = 1, shuffle=True)
+
+    return train_dataloader, val_dataloader, src_tokenizer, tgt_tokenizer
